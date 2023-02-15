9e1d1b975c3e3fac54096512ce1f438809394921
ajaye-g <ajaye.g@techversantinfo.com>
2023-02-15 18:38:32 +0530

    .

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -54,8 +54,14 @@ const Cart = () => {
                             <img src={cartItem.image} alt={cartItem.title}></img>
                             <div>
                                 <h3>{cartItem.title}</h3>
-                                <h4>{cartItem.category}</h4>
-                                <p>{cartItem.description}</p>
+                                <div className="cartRating">
+                                <h4 className="dataCategory">{cartItem.category}</h4>
+                                <span className='button1 m-2 p-1' ><b>{cartItem.rating.rate}</b>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                </svg></span>
+                                </div>
+                                <p className="dataCategory">{cartItem.description}</p>
                                 <button onClick={()=>handleRemoveFromCart(cartItem)}>Remove</button>
                             </div>
                         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,12 +29,16 @@ const Home = () => {
             <Card  className='cardWidth m-3 p-3 ' >
               <Card.Img className='img'  variant="top" src={data.image} />
               <Card.Body>
-                <Card.Title>{data.title}</Card.Title>
-                <Card.Text>$ {data.price}</Card.Text>
-                <Card.Text >{data.category}</Card.Text>
-                <Card.Text><span className='button1 m-2 p-1' >{data.rating.rate}</span></Card.Text>
+                <Card.Title><b>{data.title}</b></Card.Title>
+                <Card.Text><b>$ {data.price}</b></Card.Text>
+                <Card.Text ><b className="dataCategory">{data.category}</b></Card.Text>
+                <Card.Text><span className='button1 m-2 p-1' ><b>{data.rating.rate}</b>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                </svg>
+                </span>
+                </Card.Text>
                 <Button onClick={()=>handleAddToCart(data)}>Add to Cart</Button>
-                {/* <Button variant="primary">Add to Cart</Button> */}
               </Card.Body>
             </Card>
             </div>
